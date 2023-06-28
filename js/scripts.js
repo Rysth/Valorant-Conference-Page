9ee1bc4a17660869b51c2dcac5549ef7c905ace5
Rysth <johnpalacios.t@gmail.com>
2023-06-28 14:38:59 -0500

    Add X icon to the navigation button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 const navigationLinks = document.querySelectorAll('.nav-item');
 const menuToggle = document.getElementById('navbarText');
+const buttonToggle = document.querySelector('#navbar-toggler');
+const buttonIcon = document.querySelector('#navbar-icon');
+
+buttonToggle.addEventListener('click', () => {
+  buttonIcon.classList.toggle('fa-xmark');
+});
+
 navigationLinks.forEach((element) => {
   element.addEventListener('click', () => {
     menuToggle.classList.remove('show');
