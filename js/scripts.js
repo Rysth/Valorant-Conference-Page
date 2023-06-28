0d05691dc7e0769db65bbb22ad12f7f62fb220a2
Rysth <johnpalacios.t@gmail.com>
2023-06-28 14:42:45 -0500

    Fix Linters errors.

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,7 @@ buttonToggle.addEventListener('click', () => {
 navigationLinks.forEach((element) => {
   element.addEventListener('click', () => {
     menuToggle.classList.remove('show');
+    if (buttonIcon.classList.contains('fa-xmark'))
+      buttonIcon.classList.remove('fa-xmark');
   });
 });
